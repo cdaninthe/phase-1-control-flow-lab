3212f4036e797c0c021d825d4a53733068253740
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2021-12-20 11:39:46 -0500

    commit tree 7ce13696fbab43937b4a72fb50a062ed05fa511f

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let message;
+  if (ride > 2500) {
+    message = "No can do.";
+  }
+  else if (ride > 2000) {
+    message = 'I will gladly take your thirty bucks.';
+  }
+  else if (ride <=400) {
+    message = "This one is on me!";
+  }
+
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const message = city === 'NYC' ? ('Ok, sounds good.') : ('No go.');
+  return message;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  switch (tip){
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = 'Bye.';
+      break;
+  }
+  return message;
 }
